297ce636b99db749c2cf122ab8388bcb8206b79c
Ahmed Hodiani <a.hodiani@hotmail.com>
2024-09-09 13:55:09 +0300

    feat: printed counter

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ataher <ataher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 13:41:49 by ataher            #+#    #+#             */
-/*   Updated: 2024/09/09 10:36:21 by ataher           ###   ########.fr       */
+/*   Updated: 2024/09/09 10:59:42 by ataher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,90 +28,120 @@ size_t	ft_strlen(char const *s)
 	return (i);
 }
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putchar_fd(char c, int fd)
 {
-	write (fd, &c, 1);
+	return (write(fd, &c, 1))
 }
 
-void	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	return (write(fd, s, ft_strlen(s)));
 }
 
-void	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(int n, int fd)
 {
+	int count;
+
+	count = 0;
 	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
+		return (ft_putstr_fd("-2147483648", fd));
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
+		if (ft_putchar_fd('-', fd) == -1)
+			return (-1);
+		count++;
 		n = -n;
 	}
 	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + '0', fd);
+	{
+		if ((count += ft_putnbr_fd(n / 10, fd)) == -1)
+			return (-1);
+	}
+	if (ft_putchar_fd(n % 10 + '0', fd) == -1)
+		return (-1);
+	count++;
+	return (count);
 }
 
-static	void	ft_putaddress_fd(void *address, int fd) {
-    unsigned long addr = (unsigned long)address;
+static	int	ft_putaddress_fd(void *address, int fd)
+{
+	unsigned long addr = (unsigned long)address;
 
-    char buffer[20];
-    char *hex_chars = "0123456789abcdef";
-    int i = sizeof(buffer) - 1;
+	char buffer[20];
+	char *hex_chars = "0123456789abcdef";
+	int i = sizeof(buffer) - 1;
 
-    buffer[i--] = '\n';
+	buffer[i--] = '\n';
 
-    while (addr > 0)
-    {
-        buffer[i--] = hex_chars[addr % 16];
-        addr /= 16;
-    }
+	while (addr > 0)
+	{
+		buffer[i--] = hex_chars[addr % 16];
+		addr /= 16;
+	}
+	buffer[i--] = 'x';
+	buffer[i] = '0';
 
-    buffer[i--] = 'x';
-    buffer[i] = '0';
-
-    write(fd, &buffer[i], sizeof(buffer) - i);
+	return (write(fd, &buffer[i], sizeof(buffer) - i));
 }
 
 int	ft_printf(const char *format, ...)
 {
-	ft_putchar_fd('\n', 1);
-	ft_putchar_fd('\n', 1);
-
 	va_list	inputs;
 	va_start(inputs, format);
 
 	int i = 0;
+	int count = 0;
 	while(format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == 'd')
-				ft_putnbr_fd(va_arg(inputs, int), 1);
+			{
+				int len = ft_putnbr_fd(va_arg(inputs, int), 1);
+				if (len == -1)
+					return count;
+				count += len;
+			}
 			if (format[i] == 's')
-				ft_putstr_fd(va_arg(inputs, char *), 1);
+			{
+				int len = ft_putstr_fd(va_arg(inputs, char *), 1);
+				if (len == -1)
+					return count;
+				count += len;
+			}
 			if (format[i] == 'c')
-				ft_putchar_fd(va_arg(inputs, int), 1);
+			{
+				if (ft_putchar_fd(va_arg(inputs, int), 1) == -1)
+					return count;
+				count += 1;
+			}
 			if (format[i] == 'p')
-				ft_putaddress_fd((void *)va_arg(inputs, void *), 1);
+			{
+				int len = ft_putaddress_fd((void *)va_arg(inputs, void *), 1);
+				if (len == -1)
+					return count;
+				count += len;
+			}
 
 
 			if (format[i] == '%')
-				ft_putchar_fd('%', 1);
+			{
+				if (ft_putchar_fd('%', 1) == -1)
+					return count;
+				count += 1;
+			}
 		}
 		else
-			ft_putchar_fd(format[i], 1);
+		{
+			if (ft_putchar_fd(format[i], 1) == -1)
+				return count;
+			count += 1;
+		}
 		i++;
 	}
-
-	ft_putchar_fd('\n', 1);
-	ft_putchar_fd('\n', 1);
 	va_end(inputs);
-	return (0);
+	return (count);
 }
 
 
