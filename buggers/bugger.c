d688085786d12353df9462fd98448c4d35db9635
ahmedhodiani <a.hodiani@hotmail.com>
2024-11-24 09:57:09 +0300

    fix: norm

--- buggers/bugger.c
+++ buggers/bugger.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bugger.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ataher <ataher@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 09:52:34 by ataher            #+#    #+#             */
+/*   Updated: 2024/11/24 09:54:09 by ataher           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/ft_printf.h"
 
-static void    hp_bugger(char *format, va_list inputs)
+static void	hp_bugger(char *format, va_list inputs)
 {
 	int		i;
 
@@ -16,89 +28,88 @@ static void    hp_bugger(char *format, va_list inputs)
 	va_end(inputs);
 }
 
-void bugger_cmd(char *format, ...)
+void	bugger_cmd(char *format, ...)
 {
 	va_list	inputs;
-	va_start(inputs, format);
 
-    ft_printf(BLUE);
-    ft_printf(">> ");
-    ft_printf(RESET);
-    hp_bugger(format, inputs);
-    ft_printf("\n");
+	va_start(inputs, format);
+	ft_printf(BLUE);
+	ft_printf(">> ");
+	ft_printf(RESET);
+	hp_bugger(format, inputs);
+	ft_printf("\n");
 }
 
-void bugger_info(char *format, ...)
+void	bugger_info(char *format, ...)
 {
 	va_list	inputs;
-	va_start(inputs, format);
 
-    ft_printf(GRAY);
-    ft_printf("[INFO]>> ");
-    ft_printf(RESET);
-    hp_bugger(format, inputs);
-    ft_printf("\n");
+	va_start(inputs, format);
+	ft_printf(GRAY);
+	ft_printf("[INFO]>> ");
+	ft_printf(RESET);
+	hp_bugger(format, inputs);
+	ft_printf("\n");
 }
 
-
-void bugger_error(char *format, ...)
+void	bugger_error(char *format, ...)
 {
 	va_list	inputs;
+
 	va_start(inputs, format);
-
-    ft_printf(RED);
-    ft_printf("[ERROR]>> ");
-    hp_bugger(format, inputs);
-    ft_printf(RESET);
-    ft_printf("\n");
+	ft_printf(RED);
+	ft_printf("[ERROR]>> ");
+	hp_bugger(format, inputs);
+	ft_printf(RESET);
+	ft_printf("\n");
 }
 
-void bugger_box(char *format, ...)
+void	bugger_box(char *format, ...)
 {
-    va_list	inputs;
-    va_start(inputs, format);
+	va_list	inputs;
 
-    ft_printf(GRAY);
-    ft_printf("╔═══════════════════════##-\n");
-    ft_printf("║ \n");
-    ft_printf("║ ");
-    ft_printf(RESET);
-    hp_bugger(format, inputs);
-    ft_printf(GRAY);
-    ft_printf("\n║");
-    ft_printf("\n╚═══════════════════════##-\n");
-    ft_printf(RESET);
+	va_start(inputs, format);
+	ft_printf(GRAY);
+	ft_printf("╔═══════════════════════##-\n");
+	ft_printf("║ \n");
+	ft_printf("║ ");
+	ft_printf(RESET);
+	hp_bugger(format, inputs);
+	ft_printf(GRAY);
+	ft_printf("\n║");
+	ft_printf("\n╚═══════════════════════##-\n");
+	ft_printf(RESET);
 }
 
-void bugger_cute(char *format, ...)
-{
-    va_list	inputs;
-    va_start(inputs, format);
+// void	bugger_cute(char *format, ...)
+// {
+// 	va_list	inputs;
 
-    ft_printf(MAGENTA);
-    ft_printf("[CUTE 🍇]>> ");
-    hp_bugger(format, inputs);
-    ft_printf(RESET);
-    ft_printf("\n");
-}
+// 	va_start(inputs, format);
+// 	ft_printf(MAGENTA);
+// 	ft_printf("[CUTE 🍇]>> ");
+// 	hp_bugger(format, inputs);
+// 	ft_printf(RESET);
+// 	ft_printf("\n");
+// }
 
-void bugger_warning(char *format, ...)
-{
-    va_list	inputs;
-    va_start(inputs, format);
+// void	bugger_warning(char *format, ...)
+// {
+// 	va_list	inputs;
 
-    ft_printf(BRIGHT_YELLOW);
-    ft_printf("[WARNING]>> ");
-    hp_bugger(format, inputs);
-    ft_printf(RESET);
-    ft_printf("\n");
-}
+// 	va_start(inputs, format);
+// 	ft_printf(BRIGHT_YELLOW);
+// 	ft_printf("[WARNING]>> ");
+// 	hp_bugger(format, inputs);
+// 	ft_printf(RESET);
+// 	ft_printf("\n");
+// }
 // int main()
 // {
-//     bugger_cmd("hello %s %d", "world", 23);
-//     bugger_info("hello %s %d", "world", 23);
-//     bugger_error("hello %s %d", "world", 23);
-//     bugger_warning("hello %s %d", "world", 23);
-//     bugger_cute("hello %s %d", "world", 23);
-//     bugger_box("I am a box %s %d", "world", 23);
+//	 bugger_cmd("hello %s %d", "world", 23);
+//	 bugger_info("hello %s %d", "world", 23);
+//	 bugger_error("hello %s %d", "world", 23);
+//	 bugger_warning("hello %s %d", "world", 23);
+//	 bugger_cute("hello %s %d", "world", 23);
+//	 bugger_box("I am a box %s %d", "world", 23);
 // }
